b1ff0182be09c89fd97c03b4e400ef9e47a9649d
julianmaster <julienm86230@hotmail.fr>
2017-11-21 00:08:42 +0100

    Start land screen

---
+++ core/src/com/asciiprogressheroes/game/view/LandScreen.java
@@ -0,0 +1,17 @@
+package com.asciiprogressheroes.game.view;
+
+import com.asciiprogressheroes.game.AsciiProgressHeroes;
+
+public class LandScreen extends CommonScreen {
+
+    public LandScreen(final AsciiProgressHeroes game) {
+        super(game);
+    }
+
+    @Override
+    public void render(float delta) {
+        // make the automatic combat at speed time
+
+        // Show informations
+    }
+}
